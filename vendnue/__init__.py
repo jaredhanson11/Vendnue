cda9b6e04a2ffa4870ad4b263170e908f0e5a6b3
Jared Hanson <hansonj@mit.edu>
2017-01-11 20:23:09 -0800

    Added secret key

--- vendnue/__init__.py
+++ vendnue/__init__.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://hansonj:password@sql.mit.edu/hansonj+vendnue'
+app.secret_key = 'vCasCy0yNdHxvTT30IHOEfqvbUaKON9ulvaS3EssxKhVm8deZMgsaWlmcyQKO1Qn5cy2Z'
 db = SQLAlchemy(app)
 api = Api(app)
 
