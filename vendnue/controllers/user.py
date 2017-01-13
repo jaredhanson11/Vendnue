3c63a0c15c611018dc3bdf4529b9817a1179184d
Jared Hanson <hansonj@mit.edu>
2017-01-12 21:29:27 -0800

    Added intiial get /user/<id> endpoint

---
+++ vendnue/controllers/user.py
@@ -0,0 +1,21 @@
+from flask import request, session
+from flask_restful import Resource
+from flask_login import login_required, current_user
+from ..models import user
+from ..utils import *
+
+class User(Resource):
+    decorators = [login_required]
+
+    def get(self, id):
+        # You can only get the information for your own profile
+        if current_user.id != id:
+            return responses.error('Permission denied', 403)
+
+        user_data = {
+                'email': current_user.email,
+                'first_name': current_user.first_name,
+                'last_name': current_user.last_name
+            }
+
+        return responses.success(user_data, 200)
--- vendnue/routes.py
+++ vendnue/routes.py
@@ -1,7 +1,8 @@
 from . import api
-from controllers import auth
+from controllers import auth, user
 
 def add_resources():
     api.add_resource(auth.Signup, '/auth/signup')
     api.add_resource(auth.Login, '/auth/login')
     api.add_resource(auth.Logout, '/auth/logout')
+    api.add_resource(user.User, '/user/<int:id>')
