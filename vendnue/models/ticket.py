263df05ecdb9e321d15a47822e5de36738470c66
John Clarke <jaclarke@mit.edu>
2017-01-17 13:37:52 -0800

    create ticket method added

--- vendnue/models/ticket.py
+++ vendnue/models/ticket.py
@@ -13,3 +13,16 @@ class Ticket(db.Model):
     seller_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     # concert through backref
     # section through backref
+
+    @staticmethod
+    def create_ticket(concert_id, section_id, price, seller_id):
+        new_ticket = Ticket(concert_id=concert_id, section_id=section_id, price=price, seller_id=seller_id)
+        db.session.add(new_ticket)
+        db.session.flush()
+        new_ticket.path_to_tickets = '/' + str(new_ticket.id)
+        db.session.add(new_ticket)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            return False
+        return True
