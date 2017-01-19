0c8ea197db1596e1e5e6995eb24217d7a9aed0bf
John Clarke <jaclarke@mit.edu>
2017-01-19 14:02:24 -0800

    separated section_bid_match into modular functions

--- vendnue/models/exchange.py
+++ vendnue/models/exchange.py
@@ -2,58 +2,83 @@ from sqlalchemy.exc import IntegrityError
 from section_bid import Section_Bid
 from ticket import Ticket
 
+from copy import copy
+
 from cleared_section_bid import Cleared_Section_Bid
 from sold_ticket import Sold_Ticket
 
 from . import db
 
-def section_bid_match(section_obj):
-    section_bid_obj_by_price_date = section_obj.bids \
-        .order_by(Section_Bid.bid_price_per_ticket.desc()) \
-        .order_by(Section_Bid.created_at.asc())
+SA_INSTANCE_STATE = '_sa_instance_state'
 
-    for section_bid_obj in section_bid_obj_by_price_date:
+def get_attributes(obj):
+    obj_attr = copy(obj.__dict__)
+    obj_attr.pop(SA_INSTANCE_STATE)
+    return obj_attr
+
+def create_cleared_section_bid(section_bid_obj):
+
+    section_bid_attr = get_attributes(section_bid_obj)
+
+    if 'section' in section_bid_attr:
+        print 'This should never be here'
+        print '----------ERROR ERROR ERROR CHECK ME----------------'
+        section_bid_attr.pop('section')
+
+    new_cleared_section_bid_obj = Cleared_Section_Bid(**section_bid_attr)
+    db.session.add(new_cleared_section_bid_obj)
+    db.session.delete(section_bid_obj)
+
+def create_sold_tickets(ticket_ids_list, section_bid_id):
+    ticket_objs_list = Ticket.query.filter(Ticket.id.in_(ticket_ids_list)).all()
+    for ticket_obj in ticket_objs_list:
+        ticket_obj_attr = get_attributes(ticket_obj)
+        ticket_obj_attr['cleared_section_bid_id'] = section_bid_id
+        new_sold_ticket_obj = Sold_Ticket(**ticket_obj_attr)
+        db.session.add(new_sold_ticket_obj)
+        db.session.delete(ticket_obj)
+
+# cache stores already searched lowest ask by num tix {num_tix : lowest_ask...}
+def match(section_bid_obj, cache):
+    num_tickets = section_bid_obj.num_tickets
+
+    if num_tickets not in cache:
         try:
-            ask_price_avg, ticket_ids_list = get_lowest_ask_by_num_tix(section_bid_obj.num_tickets)
+            ask_price_avg, ticket_ids_list = get_lowest_ask_by_num_tix(num_tickets)
         except:
             print 'There were not enough tickets in the db'
             return
-        from copy import deepcopy
-        if section_bid_obj.bid_price_per_ticket >= ask_price_avg:
-            section_bid_attr = deepcopy(section_bid_obj.__dict__)
-            ## Getting all the columns/values
-            section_bid_attr.pop('_sa_instance_state')
-            from pprint import pprint
-            if 'section' in section_bid_attr:
-                pprint(section_bid_attr)
-                print 'This should never be here'
-                print '----------ERROR ERROR ERROR CHECK ME----------------'
-            new_cleared_section_bid_obj = Cleared_Section_Bid(**section_bid_attr)
-            db.session.add(new_cleared_section_bid_obj)
-            db.session.delete(section_bid_obj)
+        cache[num_tickets] = (ask_price_avg, ticket_ids_list)
 
+    ask_price_avg, ticket_ids_list = cache[num_tickets]
 
-            ticket_objs_list = Ticket.query.filter(Ticket.id.in_(ticket_ids_list)).all()
-            for ticket_obj in ticket_objs_list:
-                ticket_obj_attr = deepcopy(ticket_obj.__dict__)
-                ticket_obj_attr.pop('_sa_instance_state')
-                ticket_obj_attr['cleared_section_bid_id'] = section_bid_obj.id
-                new_sold_ticket_obj = Sold_Ticket(**ticket_obj_attr)
-                db.session.add(new_sold_ticket_obj)
-                db.session.delete(ticket_obj)
-            try:
-                db.session.commit()
-            except IntegrityError, e:
-                db.session.rollback()
-                print 'There was a big no no, ticket wasn\'t sold'
-                print e
-                ## some sort of critical logging here
-            print 'Sold ticket for' + str(section_obj.id)
-            return
+    if section_bid_obj.bid_price_per_ticket >= ask_price_avg:
+        create_cleared_section_bid(section_bid_obj)
+        create_sold_tickets(ticket_ids_list, section_bid_obj.id)
 
-        print 'No tickets were sold'
+        try:
+            db.session.commit()
+        except IntegrityError, e:
+            db.session.rollback()
+            print 'There was a big no no, ticket wasn\'t sold'
+            print e
+            ## some sort of critical logging here
+        print 'Sold ticket for' + str(section_bid_obj.section.id)
+        return
 
+    print 'No tickets were sold'
 
+def section_bid_match(section_obj):
+    # queries db for all section bid objects
+    # sorted by price and date
+    section_bid_obj_by_price_date = section_obj.bids \
+        .order_by(Section_Bid.bid_price_per_ticket.desc()) \
+        .order_by(Section_Bid.created_at.asc())
+    
+    # iterate through each section bid object, applying the maching process
+    cache = {}
+    for section_bid_obj in section_bid_obj_by_price_date:
+        match(section_bid_obj, cache)
 
 def get_lowest_ask_by_num_tix(num_tix):
     lowest_tickets_prices = Ticket.query.with_entities(Ticket.price, Ticket.id).order_by(Ticket.price.asc()).limit(num_tix).all()
