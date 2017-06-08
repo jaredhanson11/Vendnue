b05be1f671e8b36504ea8e3e3df08ca76a530852
Jared Hanson <hansonj@mit.edu>
2017-06-07 22:56:39 -0700

    Added database migration support

--- vendnue/__init__.py
+++ vendnue/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_login import LoginManager
 from flask_cors import CORS, cross_origin
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -11,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://hansonj:password@sql.mit.edu/ha
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'vCasCy0yNdHxvTT30IHOEfqvbUaKON9ulvaS3EssxKhVm8deZMgsaWlmcyQKO1Qn5cy2Z'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 api = Api(app)
 # Need to import routes after instatiating the api object
